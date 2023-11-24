8a5a2cdc9988fc535367dfe246d09041c5cee7e8
Oleg <kharenkoleg@gmail.com>
2023-11-24 19:03:50 +0100

    creamos clase CLientsViewModel en la carpeta ViewModel

---
+++ ClientsMVVM/ViewModel/ClientsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ClientsMVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientsMVVM.ViewModel
+{
+    public class ClientsViewModel
+    {
+        // Nuestras propiedaades
+        public ObservableCollection<Client> Clients { get; set; }
+    }
+}
