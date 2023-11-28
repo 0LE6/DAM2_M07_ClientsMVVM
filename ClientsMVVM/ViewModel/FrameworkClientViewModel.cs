1f0e04c7f987ec4632fd41fb7561df56d761dbd5
Oleg <kharenkoleg@gmail.com>
2023-11-28 18:18:17 +0100

    moviendo commands

--- ClientsMVVM/ViewModel/FrameworkClientViewModel.cs
+++ ClientsMVVM/ViewModel/FrameworkClientViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using ClientsMVVM.Model;
 using ClientsMVVM.Repositori;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -13,6 +14,13 @@ namespace ClientsMVVM.ViewModel
 {
     partial class FrameworkClientViewModel : ObservableObject
     {
+        // Copiamos esto del ClientsViewModel
+        public FrameworkClientViewModel()
+        {
+            repositoriDeClients = Repo.ObreBDClients();
+            Clients = repositoriDeClients.Obten();
+        }
+
         // Ponemos una anotación de ObservableProperty (la clase tiene que ser PARCIAL)
         [ObservableProperty]
         string nom;
@@ -34,5 +42,119 @@ namespace ClientsMVVM.ViewModel
 
         Client clientEnEdicio;
         IRepositoriDeClients repositoriDeClients;
+
+        // ------------------------------------------------------
+
+        #region CODI DELS COMMANDS
+
+        private bool PotDescartarEdicio()
+        {
+            return EstemEditant;
+        }
+
+        private bool PotConfirmarEdicio()
+        {
+            return EstemEditant;
+        }
+
+        private bool PotEditarClient()
+        {
+            return !EstemEditant && Posicio != -1; // si no estamos editando, se puede editar
+        }
+
+        private bool PotAfegirClient()
+        {
+            return EsValid && true;
+        }
+
+        private bool PotEliminarClient()
+        {
+            return Posicio != -1;
+        }
+
+        private bool PotCrearClients()
+        {
+            return Clients.Count == 0;
+        }
+
+        private void DescartaEdicio()
+        {
+            EstemEditant = false;
+            Nom = ""; Cognom = ""; Saldo = "";
+        }
+
+        private void ConfirmaEdicio()
+        {
+            // lógica de implmentación de la confirmación de un cliente
+            // usamos el clientEnEdicio
+
+            clientEnEdicio.Nom = Nom;
+            clientEnEdicio.Cognom = Cognom;
+            clientEnEdicio.Saldo = Convert.ToDecimal(Saldo);
+            repositoriDeClients.Modifica(clientEnEdicio);
+
+            // una vez modificado, se obtiene
+            Clients = repositoriDeClients.Obten();
+            EstemEditant = false;
+
+            // después borrar los datos
+            Nom = ""; Cognom = ""; Saldo = "";
+        }
+
+        private void EditaClient()
+        {
+            EstemEditant = true;
+            clientEnEdicio = new Client { Id = Clients[Posicio].Id }; // !!!
+            Nom = Clients[Posicio].Nom;
+            Cognom = Clients[Posicio].Cognom;
+            Saldo = Clients[Posicio].Saldo.ToString();
+        }
+
+        private bool EsValid
+        {
+            get
+            {
+                decimal saldo;
+
+                return (
+                    !string.IsNullOrEmpty(Nom) &&
+                    !string.IsNullOrEmpty(Cognom) &&
+                    Decimal.TryParse(Saldo, out saldo)
+                    );
+            }
+        }
+
+        private void AfageixClientNou()
+        {
+            Client clientNou = new Client()
+            {
+                Id = Guid.NewGuid().ToString(), // lol ... genera clave aleatoria
+                Nom = Nom,
+                Cognom = Cognom,
+                Saldo = Convert.ToDecimal(Saldo)
+            };
+            repositoriDeClients.Afegeix(clientNou);
+            Clients = repositoriDeClients.Obten(); // refrescar lista de clients
+        }
+
+        private void EliminaClient()
+        {
+            repositoriDeClients.Esborra(Clients[Posicio].Id);
+            if (posicio == Clients.Count - 1)
+            {
+                Posicio--; // que se quede en la anterior
+            }
+            Clients = repositoriDeClients.Obten(); // refrescar lista de clients
+        }
+
+        // Ya tenemos el command pasado
+        [RelayCommand (CanExecute = nameof(PotCrearClients))]
+        private void CreaClients(int nClients)
+        {
+            repositoriDeClients.CreaClients(nClients);
+            Clients = repositoriDeClients.Obten(); // refrescar lista de clients
+        }
+
+        #endregion
     }
 }
