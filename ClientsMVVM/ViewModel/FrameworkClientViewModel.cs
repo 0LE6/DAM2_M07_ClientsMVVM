0784d24c11219b1ab718e61c2969e4b66b0fe64f
Oleg <kharenkoleg@gmail.com>
2023-11-28 18:10:08 +0100

    partial class FrameworkClientViewModel : ObservableObject

---
+++ ClientsMVVM/ViewModel/FrameworkClientViewModel.cs
@@ -0,0 +1,38 @@
+﻿using ClientsMVVM.Model;
+using ClientsMVVM.Repositori;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClientsMVVM.ViewModel
+{
+    partial class FrameworkClientViewModel : ObservableObject
+    {
+        // Ponemos una anotación de ObservableProperty (la clase tiene que ser PARCIAL)
+        [ObservableProperty]
+        string nom;
+
+        [ObservableProperty]
+        string cognom;
+
+        [ObservableProperty]
+        string saldo;
+
+        [ObservableProperty]
+        ObservableCollection<Client> clients;
+
+        [ObservableProperty]
+        int posicio;
+
+        [ObservableProperty]
+        bool estemEditant = false; // para editar
+
+        Client clientEnEdicio;
+        IRepositoriDeClients repositoriDeClients;
+    }
+}
