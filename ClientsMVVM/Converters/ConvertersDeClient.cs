aaa6c1b84b99fe9c7888a4b37eaf5715db55e1f2
Oleg <kharenkoleg@gmail.com>
2023-11-28 17:47:59 +0100

    dsfg

--- ClientsMVVM/Converters/ConvertersDeClient.cs
+++ ClientsMVVM/Converters/ConvertersDeClient.cs
@@ -4,7 +4,9 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace ClientsMVVM.Converters
 {
@@ -12,7 +14,57 @@ namespace ClientsMVVM.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            Decimal? valor = (Decimal?)value;
+            if (valor != null)
+            {
+                if (valor < 0)
+                {
+                    return Brushes.DarkRed;
+                }
+            }
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
             throw new NotImplementedException();
+        }
+    }
+
+    public class GruixConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Decimal? valor = (Decimal?)value;
+            if (valor != null)
+            {
+                if (valor < 0)
+                {
+                    return FontWeights.Bold;
+                }
+            }
+            return FontWeights.Normal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ImatgeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Decimal? valor = (Decimal?)value;
+            if (valor != null)
+            {
+                if (valor < 0)
+                {
+                    return "../Imatges/cross1.png";
+                }
+            }
+            return "../Imatges/check1.png";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
